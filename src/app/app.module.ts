21e8f82694c8f87f3d9dc866eb1171ce97d5bed7
dominikJaworski <dominik777@outlook.com>
2019-06-12 21:40:22 -0400

    <table added>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatRadioModule, MatButtonModule, MatTabsModule, MatTableModule, MatFormFieldModule, MatOptionModule, MatSelectModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { InvestmentformComponent } from './investmentform/investmentform.component';
+import { LinechartComponent } from './linechart/linechart.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvestmentformComponent,
+    LinechartComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
